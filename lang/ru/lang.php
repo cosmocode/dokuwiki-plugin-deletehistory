2ba08a5c19b6c29f14ac050297f5814fa816d4d5
Aleksandr Selivanov <alexgearbox@yandex.ru>
2024-01-03 20:05:13 +0100

    translation update

---
+++ lang/ru/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ */
+$lang['menu']                  = 'Удаление истории редактирования';
+$lang['buttonDelete']          = 'Удалить все файлы истории редактирования';
+$lang['done']                  = 'История редактирования удалена';
