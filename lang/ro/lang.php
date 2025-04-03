7b2c3dd6f4cf9331e1c788eb040d2d8103664b7a
Tiberiu Popescu <popescu.tibi@gmail.com>
2025-04-03 16:30:05 +0200

    translation update

---
+++ lang/ro/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tiberiu Popescu <popescu.tibi@gmail.com>
+ */
+$lang['menu']                  = 'Șterge istoricul';
+$lang['buttonDelete']          = 'Șterge toate fișierele vechi';
+$lang['done']                  = 'Istoricul a fost șters';
